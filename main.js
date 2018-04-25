b519a4b14dff1e5fa7b675a9148c0acb96d4bcd7
Vikas Gola <vikasgola2015@gmail.com>
2018-04-25 15:32:12 +0530

    added mockingbird

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 
+//               L
+//              L
+//             L        ---     Calculus
+//           L  L
+//         L     L
+
 const I = a => a;
 
 // Identity of Identity is Identity
@@ -7,3 +13,15 @@ const I = a => a;
 
 // add in lambda calculus
 add = a => b => a+b;
+sub = a => b => a-b;
+mul = a => b => a*b;
+div = a => b => a/b;
+// Example: add(5)(6)
+//  mean that a = 5 and b = 6
+
+//  abstraction and beta-reduction
+//  fundamentals of lambda-calculus
+
+
+// Mockingbird
+M = f => f(f);
